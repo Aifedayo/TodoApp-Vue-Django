9584a414d594fb0789497e3ad5d9cbe3188298db
Aifedayo <akeemifedayolag@gmail.com>
2021-11-07 20:48:06 +0100

    models for vue

--- vuengo/vuengo/task/views.py
+++ vuengo/vuengo/task/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import authentication, serializers
+from rest_framework.decorators import authentication_classes, permission_classes
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class TaskViewSet(viewsets.ModelViewSet):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
